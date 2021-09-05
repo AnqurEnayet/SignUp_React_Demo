2614f03ea45265a25356718dbb834ee0b519b48b
AnqurEnayet <anqur1996@gmail.com>
2021-09-05 07:23:08 +0600

    First Commit and functional signup demo. CSS designing left.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import './components/signup/Signup';
+import Signup from './components/signup/Signup';
+import Description from './components/description/Description';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex-container">
+        <div className="left">
+          <Signup />
+        </div>
+        <div className="right">
+          <Description />
+        </div>
+
+      </div>
     </div>
   );
 }
